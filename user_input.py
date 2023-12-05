f19a13e5cd8eecb9d91ff8301c188bc9e501fccc
vvronc <ronovictor40@gmail.com>
2023-12-05 09:14:37 +0300

    lists

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+age = input("what is your age?")
+location = input("where is your location?")
+print("Hello {name} you are {age} years old, and you live in {location}")
